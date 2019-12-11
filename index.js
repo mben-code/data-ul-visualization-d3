a17611abbc83d426370460b5d529bede9b505687
smartgmb0918 <vuehero18@outlook.com>
2019-12-10 21:15:05 -0800

    Implement update function

--- index.js
+++ index.js
@@ -18,6 +18,60 @@ const xAxisGroup = graph.append('g')
 
 const yAxisGroup = graph.append('g');
 
+// scales
+const y = d3.scaleLinear()
+  .range([graphHeight, 0]);
+const x = d3.scaleBand()
+  .range([0, 500])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+// create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
+
+// update x axis text
+xAxisGroup.selectAll('text')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end')
+  .attr('fill', 'orange');
+
+// update function
+const update = (data) => {
+  //updating scale domains
+  y.domain([0, d3.max(data, d => d.orders)]);
+  x.domain(data.map(item => item.name));
+
+  // join the data to rects
+  const rects = graph.selectAll('rect')
+    .data(data);
+
+  // remove exit selection
+  rects.exit().remove();
+
+  // update current shapes in dom
+  rects.attr('width', x.bandwidth)
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+  // append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+    .attr('width', x.bandwidth)
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+    // call axes
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+}
+
 db.collection('dishes').get().then(res => {
 
   const data = [];
@@ -25,46 +79,10 @@ db.collection('dishes').get().then(res => {
     data.push(doc.data());
   });
 
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.orders)])
-    .range([graphHeight, 0]);
+  update(data);
 
-  const x = d3.scaleBand()
-    .domain(data.map(item => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
-
-  // join the data to rects
-  const rects = graph.selectAll('rect')
-    .data(data);
-
-  rects.attr('width', x.bandwidth())
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
-
-  //append the enter selection to the DOM
-  rects.enter()
-    .append('rect')
-    .attr('width', x.bandwidth())
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
-
-  // create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + ' orders');
-
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
-
-  xAxisGroup.selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('fill', 'orange');
+  d3.interval(() => {
+    data.pop();
+    update(data);
+  }, 1000);
 });
